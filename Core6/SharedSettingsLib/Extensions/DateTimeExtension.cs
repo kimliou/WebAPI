c4ed160deb6b70a8a686d945c9bb0c8f1dacacbc
Kim <kim056992@gmail.com>
2022-11-15 17:41:18 +0800

    新增Extension方法:copy物件、日期格式轉換。

--- Core6/SharedSettingsLib/Extensions/DateTimeExtension.cs
+++ Core6/SharedSettingsLib/Extensions/DateTimeExtension.cs
@@ -9,9 +9,21 @@ namespace SharedSettingsLib.Extensions
   public static class DateTimeExtension
   {
     public static string yyyy_MM_dd => "yyyy-MM-dd";
+    public static string yyyy_MM_dd_HH_mm_ss_fff => "yyyy-MM-dd HH:mm:ss.fff";
+
+
     public static string ToString_yyyy_MM_dd(this DateTime _DateTime)
     {
       return _DateTime.ToString(yyyy_MM_dd);
+    }
+    public static string ToString_yyyy_MM_dd_HH_mm_ss_fff(this DateTime _DateTime)
+    {
+      return _DateTime.ToString(yyyy_MM_dd_HH_mm_ss_fff);
+    }
+
+    public static string ToString_yyyy_MM_dd_HH_mm_ss_fff(this DateTime? _DateTime)
+    {
+      return (_DateTime.HasValue) ? _DateTime.Value.ToString(yyyy_MM_dd_HH_mm_ss_fff) : "";
     }
 
   }
--- Core6/SharedSettingsLib/Extensions/ObjectExtension.cs
+++ Core6/SharedSettingsLib/Extensions/ObjectExtension.cs
@@ -56,5 +56,116 @@ namespace SharedSettingsLib.Extensions
         : "";
     }
 
+    /// <summary>
+    /// 比對兩個物件，欄位名稱相同的將值寫入另外一個物件。
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="model"></param>
+    /// <param name="destination"></param>
+    /// <returns></returns>
+    public static string CopyTo<T>(this object model, T destination)
+    {
+      var sb = new StringBuilder();
+      if (model == null || destination == null)
+      {
+        sb.AppendLine($"object reference can not be null");
+        return sb.ToString();
+      }
+      model.GetType().GetProperties().ToList().ForEach(p => // 每一個資料欄位 _
+      {
+        try
+        {
+          var destProperty = destination.GetType().GetProperty(p.Name);
+          var destPropertyType = destProperty?.PropertyType;
+          var destPropertyTypeName = destProperty?.Name ?? "";
+          if (
+            destProperty != null && destPropertyType != null &&
+            !string.IsNullOrEmpty(destPropertyTypeName) && p.Name == destPropertyTypeName
+          )
+          {
+            if (destPropertyType.IsGenericType && destPropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)) // check if nullable _
+            {
+              destPropertyType = destPropertyType.GetGenericArguments()[0];
+            }
+            var sourceValue = p.GetValue(model);
+            // if (sourceValue != null) // 值不為 NULL 才複製 
+            {
+              var sourcePropertyType = p.PropertyType;
+              if (sourcePropertyType.IsGenericType && sourcePropertyType.GetGenericTypeDefinition() == typeof(Nullable<>)) // check if nullable _
+              {
+                sourcePropertyType = sourcePropertyType.GetGenericArguments()[0];
+              }
+              var sourceTypeIsSystemDateTime = false;
+              if (sourcePropertyType?.ToString() == typeof(DateTime).ToString())
+              {
+                sourceTypeIsSystemDateTime = true;
+                if (sourceValue != null)
+                {
+                  var datetimeValue = (DateTime)sourceValue;
+                  sb.AppendLine($"ToString: {datetimeValue.ToString()}, ToUniversalTime: {datetimeValue.ToUniversalTime().ToString_yyyy_MM_dd_HH_mm_ss_fff()}, ToLocalTime: {datetimeValue.ToLocalTime().ToString_yyyy_MM_dd_HH_mm_ss_fff()}");
+                }
+              }
+
+              if (destProperty != null && destPropertyType != null && sourcePropertyType == destPropertyType) // source type == destination type 
+              {
+                try
+                {
+                  destProperty?.SetValue(destination,
+                    (sourceTypeIsSystemDateTime ? ((DateTime?)sourceValue) : sourceValue)
+                  );
+                }
+                catch (Exception ex)
+                {
+                  sb.AppendLine($"{p.Name}: {ex.GetType().Name}: {ex.Message}");
+                }
+              }
+              else if (destPropertyType?.Name == typeof(string).Name)
+              {
+                try
+                {
+                  destProperty?.SetValue(destination,
+                    (sourceTypeIsSystemDateTime ? ((DateTime?)sourceValue).ToString_yyyy_MM_dd_HH_mm_ss_fff() : sourceValue.ToString())
+                  );
+                }
+                catch (Exception ex)
+                {
+                  sb.AppendLine($"{p.Name}: {ex.GetType().Name}: {ex.Message}");
+                }
+              }
+            }
+          }
+        }
+        catch (Exception ex)
+        {
+          sb.AppendLine($"{ex.GetType().Name}: {ex.Message}");
+        }
+      });
+      return sb.ToString();
+    }
+
+    /// <summary>
+    /// 根據Property名稱取值，發生錯誤時回傳空白
+    /// </summary>
+    /// <param name="input"></param>
+    /// <param name="PropName">Property名稱</param>
+    /// <returns></returns>
+    public static string GetPropValue(this object input, string PropName)
+    {
+      string result = "";
+      try
+      {
+        var type = input.GetType();
+        var prop = input.GetType().GetProperty(PropName);
+        if (prop != null)
+        {
+          result = prop.GetValue(input, null)!.ToString()!;
+        }
+      }
+      catch (Exception)
+      {
+        result = "";
+      }
+      return result;
+    }
   }
 }
