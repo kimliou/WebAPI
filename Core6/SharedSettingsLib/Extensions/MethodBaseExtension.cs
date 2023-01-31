8610a1b9aec7347326ecf5c0d06321d5a6dfa659
Kim <kim056992@gmail.com>
2023-01-31 11:20:49 +0800

    MethodBase 簡化取Method名稱

---
+++ Core6/SharedSettingsLib/Extensions/MethodBaseExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedSettingsLib.Extensions
+{
+  public static class MethodBaseExtension
+  {
+    public static string GetMethodInfo(this MethodBase? methodBase)
+    {
+      return $"{methodBase?.ReflectedType?.Namespace}.{methodBase?.ReflectedType?.Name}.{methodBase?.Name}";
+    }
+  }
+}
