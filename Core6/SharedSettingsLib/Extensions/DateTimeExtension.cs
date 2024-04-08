d2603ded5cc52f04358f0ba1b4331f0408bfa753
Kim <kim056992@gmail.com>
2024-04-08 10:46:42 +0800

    更新日期算法 方法

--- Core6/SharedSettingsLib/Extensions/DateTimeExtension.cs
+++ Core6/SharedSettingsLib/Extensions/DateTimeExtension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +26,20 @@ namespace SharedSettingsLib.Extensions
     {
       return (_DateTime.HasValue) ? _DateTime.Value.ToString(yyyy_MM_dd_HH_mm_ss_fff) : "";
     }
-
+    public static string ToString_DateFormat(this DateTime _DateTime, string Format)
+    {
+      //判斷是否為民國年，決定要不要讀取日曆套件
+      int FormatCount = Format.Count(f => f == 'y');
+      if (FormatCount == 3)
+      {
+        CultureInfo culture = new CultureInfo("zh-TW");
+        culture.DateTimeFormat.Calendar = new TaiwanCalendar();
+        return _DateTime.ToString(Format, culture);
+      }
+      else
+      {
+        return _DateTime.ToString(Format);
+      }
+    }
   }
 }
