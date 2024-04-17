8170a0fcb5a89c4980bfbc196a762866af5b0806
Kim <kim056992@gmail.com>
2024-04-17 16:20:47 +0800

    新增使用lock功能

---
+++ Core6/BLLLib/Services/LockService.cs
@@ -0,0 +1,29 @@
+﻿using SharedSettingsLib.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLLLib.Services;
+
+public interface ILockService
+{
+  void Start();
+}
+[Inject]
+public class LockService : ILockService
+{
+  public static object executionObject = new object();
+  public void Start()
+  {
+    lock (executionObject)
+    {
+      CreateSingleton();
+    }
+  }
+  public void CreateSingleton()
+  {
+
+  }
+}
---
+++ Core6/WebAPI/Controllers/LockController.cs
@@ -0,0 +1,27 @@
+﻿using BLLLib.Services;
+using BLLLib.Services.DB;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedSettingsLib.Models.DB;
+using SharedSettingsLib.Models.Public;
+
+namespace WebAPI.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class LockController : ControllerBase
+  {
+    public LockController(ILockService lockService)
+    {
+      LockService = lockService;
+    }
+    ILockService LockService { get; set; }
+
+    [HttpGet]
+    public IActionResult LockDemo()
+    {
+      LockService.Start();
+      return Ok();
+    }
+  }
+}
